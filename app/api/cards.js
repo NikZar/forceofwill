599701d115cfa90fd865d774804f26f67e1610a0
NikZar <niko.zarzani@gmail.com>
2014-10-06 22:28:07 +0200

    Facebook login validation

--- app/api/cards.js
+++ app/api/cards.js
@@ -1,10 +1,48 @@
 var express = require('express');
 var router = express.Router();
+var https = require('https');
 
+// route middleware to validate :token
+router.param('token', function(req, res, next, token) {
+
+	//Doing token validations on :token
+	var callback = function(response) {
+	  
+	  var str = '';
+	  response.on('data', function (chunk) {
+	    str += chunk;
+	  });
+	  response.on('end', function () {
+	    var fbauth = JSON.parse(str);
+		// go to the next thing
+		if(fbauth.id){
+			// once validation is done save the new item in the req
+			req.token = token;
+			// retrieving id parameter
+			req.id = fbauth.id;
+			next();
+		}
+	  });
+	}
+
+	//Checking FB API for token correctness
+	//https://graph.facebook.com/me?access_token={accessToken}
+	var options = {
+	  host: 'graph.facebook.com',
+	  path: '/me?access_token='+token,
+	  method: 'GET'
+	};
+
+	https.request(options, callback).end();
+
+});
+
+/*
 /*
  * GET cards.
  */
-router.get('', function(req, res) {
+router.get('/:token/', function(req, res) {
+	console.log("User ID: ", req.id);
     var db = req.db;
     db.collection('cards').find().toArray(function (err, items) {
         res.json(items);
