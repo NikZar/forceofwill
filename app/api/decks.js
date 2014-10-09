4f44aefbfca35a49cd7e5897eed8204e7a67563a
NikZar <niko.zarzani@gmail.com>
2014-10-10 01:13:12 +0200

    User fb authentication refactoring

--- app/api/decks.js
+++ app/api/decks.js
@@ -3,31 +3,24 @@ var router = express.Router();
 var https = require('https');
 
 
-var getAllCards = function(req, res){
+var getAllDecks = function(req, res){
 	console.log("User ID: ", req.userId);
 	console.log("App ID: ", req.appId);
     var db = req.db;
-    db.collection('cards').find().toArray(function (err, items) {
+    db.collection('decks').find().toArray(function (err, items) {
         res.json(items);
     });
 }
 
 /*
- * GET all cards.
+ * GET all decks.
  */
 router.get('', function(req, res) {
-	getAllCards(req, res);
+	getAllDecks(req, res);
 });
 
 router.get('/', function(req, res) {
-	getAllCards(req, res);
-});
-
-/*
- * GET one cards.
- */
-router.get('/:code', function(req, res) {
-	getAllCards(req, res);
+	getAllDecks(req, res);
 });
 
 module.exports = router;
